52eff6206cd51459722b59e37b99f8db9e4bb595
pratincola <pratik.patrick@gmail.com>
2014-04-06 14:39:51 -0500

    Initial commit

---
+++ src/WordCount.java
@@ -0,0 +1,82 @@
+/**
+ * Created by prateek on 3/29/14.
+ */
+
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.filecache.DistributedCache;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapred.*;
+import org.apache.hadoop.util.*;
+
+public class WordCount {
+
+    public static class Map extends MapReduceBase implements Mapper<LongWritable, Text, Text, IntWritable> {
+        private final static IntWritable one = new IntWritable(1);
+        private Text word = new Text();
+
+        public void map(LongWritable key, Text value, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            String line = value.toString();
+            StringTokenizer tokenizer = new StringTokenizer(line);
+            while (tokenizer.hasMoreTokens()) {
+                word.set(tokenizer.nextToken());
+                output.collect(word, one);
+            }
+        }
+    }
+
+    public static class Reduce extends MapReduceBase implements Reducer<Text, IntWritable, Text, IntWritable> {
+        public void reduce(Text key, Iterator<IntWritable> values, OutputCollector<Text, IntWritable> output, Reporter reporter) throws IOException {
+            int sum = 0;
+            while (values.hasNext()) {
+                sum += values.next().get();
+            }
+            output.collect(key, new IntWritable(sum));
+        }
+    }
+
+    private boolean caseSensitive = true;
+    private String inputFile;
+
+    public void configure(JobConf job) {
+        caseSensitive = job.getBoolean("analyzer.case.sensitive", true);
+        inputFile = job.get("map.input.file");
+        Set<String> patternsToSkip = new HashSet<String>();
+        patternsToSkip.add("\\.");
+        patternsToSkip.add("\\,");
+        patternsToSkip.add("\\!");
+//        patternsToSkip.add("\"");
+    }
+
+
+
+    public static void main(String[] args) throws Exception {
+        JobConf conf = new JobConf(WordCount.class);
+        conf.setJobName("wordcount");
+
+        conf.setOutputKeyClass(Text.class);
+        conf.setOutputValueClass(IntWritable.class);
+
+        conf.setMapperClass(Map.class);
+        conf.setCombinerClass(Reduce.class);
+        conf.setReducerClass(Reduce.class);
+
+        conf.setInputFormat(TextInputFormat.class);
+        conf.setOutputFormat(TextOutputFormat.class);
+
+        FileInputFormat.setInputPaths(conf, new Path(args[0]));
+        FileOutputFormat.setOutputPath(conf, new Path(args[1]));
+
+        // Disregard special characters
+        conf.setBoolean("analyzer.skip.patterns", true);
+
+        JobClient.runJob(conf);
+
+
+
+
+    }
+}
